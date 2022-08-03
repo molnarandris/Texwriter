60aa5c3b315b4188313afcca808a6d82258b35be
Andras Molnar <molnarandris@gmail.com>
2022-08-03 10:52:36 +0200

    Autocomplete ui rewrite to popover autohide false

--- src/autocomplete.py
+++ src/autocomplete.py
@@ -5,17 +5,14 @@ class AutocompletePopover(Gtk.Popover):
 
     def __init__(self, editor_page):
         super().__init__()
+
+        self.active = False
+        self.string = ""
         self.set_parent(editor_page)
         self.editor_page = editor_page
 
-        #layout = Gtk.BinLayout()
-        #self.set_layout_manager(layout)
-
         box = Gtk.Box.new(Gtk.Orientation.VERTICAL,10)
         self.set_child(box)
-        text = Gtk.Text()
-        box.append(text)
-        #text.set_visible(False)
         label = Gtk.Label.new("Hello world")
         box.append(label)
         label = Gtk.Label.new("Hello world")
@@ -24,25 +21,33 @@ class AutocompletePopover(Gtk.Popover):
         box.append(label)
         self.view = editor_page.sourceview
         self.set_position(Gtk.PositionType.BOTTOM)
-        self.set_autohide(True)
+        self.set_autohide(False)
         self.set_has_arrow(False)
         self.set_offset(0,5)
-
-        text.get_buffer().connect("inserted-text", self.on_insert)
-        self.text = text
 
         eck = Gtk.EventControllerKey.new()
         self.view.add_controller(eck)
         eck.connect("key-released", self.on_key_released)
 
-    def on_insert(self, entrybuffer, pos, string, len_):
-        #print("Hi", string)
-        pass
+        buf = self.view.get_buffer()
+        buf.connect("insert-text", self.on_insert)
+
+    def on_insert(self, buffer, it, string, len_):
+        if not self.active:
+            return
+        self.string += string
+        print("Hi", self.string)
 
     def on_key_released(self, controller, keyval, keycode, modifier):
-        if keyval is Gdk.KEY_backslash:
-            self.update_position()
+        if keyval == Gdk.KEY_backslash:
             self.popup()
+            self.active = True
+        if keyval == Gdk.KEY_Escape:
+            self.active = False
+            self.string = ""
+            self.popdown()
+        if self.active:
+            self.update_position()
 
     def update_position(self):
         buf = self.view.get_buffer()
