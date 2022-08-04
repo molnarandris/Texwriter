b2382e23871bae72e1201b19bd106be9f48e3956
Andras Molnar <molnarandris@gmail.com>
2022-08-04 16:10:04 +0200

    Rewriting log parser

--- src/logprocessor.py
+++ src/logprocessor.py
@@ -8,13 +8,10 @@ re_badbox = re.compile(r"(Ov|Und)erfull \\[hv]box ")
 re_line = re.compile(r"(l\.(?P<line>[0-9]+)( (?P<code>.*))?$|<\*>)")
 re_cseq = re.compile(r".*(?P<seq>(\\|\.\.\.)[^ ]*) ?$")
 re_macro = re.compile(r"^(?P<macro>\\.*) ->")
-re_atline = re.compile(
-"( detected| in paragraph)? at lines? (?P<line>[0-9]*)(--(?P<last>[0-9]*))?")
-re_reference = re.compile("LaTeX Warning: Reference `(?P<ref>.*)' \
-on page [0-9]* undefined on input line (?P<line>[0-9]*)\\.$")
+re_atline = re.compile("( detected| in paragraph)? at lines? (?P<line>[0-9]*)(--(?P<last>[0-9]*))?")
+re_reference = re.compile("LaTeX Warning: Reference `(?P<ref>.*)' on page [0-9]* undefined on input line (?P<line>[0-9]*)\\.$")
 re_label = re.compile("LaTeX Warning: (?P<text>Label .*)$")
-re_warning = re.compile(
-"(LaTeX|Package)( (?P<pkg>.*))? Warning: (?P<text>.*)$")
+re_warning = re.compile("(LaTeX|Package)( (?P<pkg>.*))? Warning: (?P<msg>.*)$")
 re_online = re.compile("(; reported)? on input line (?P<line>[0-9]*)")
 re_ignored = re.compile("; all text was ignored after line (?P<line>[0-9]*).$")
 
@@ -24,7 +21,7 @@ class LogProcessor(GObject.GObject):
     def __init__(self):
         super().__init__()
         self.file = None
-        self.lines = []
+        self.line_iter = None
         self.error_list = []
         self.warning_list = []
         self.badbox_list = []
@@ -46,16 +43,15 @@ class LogProcessor(GObject.GObject):
         except UnicodeDecodeError:
             print("Error: Unknown character encoding of the log file. Expecting UTF-8")
         else:
-            self.lines = log.splitlines()
+            self.line_iter = iter(log.splitlines())
             for elem in self.parse():
-                if elem["kind"] == "error":
+                if elem["type"] == "error":
                     self.error_list.append(elem)
-                if elem["kind"] == "warning" or elem["kind"] == "ref":
+                if elem["type"] == "warning" or elem["type"] == "reference":
                     self.warning_list.append(elem)
-                if elem["kind"] == "badbox":
+                if elem["type"] == "badbox":
                     self.badbox_list.append(elem)
                 print(elem)
-            print(self.badbox_list)
         finally:
             callback()
 
@@ -67,55 +63,75 @@ class LogProcessor(GObject.GObject):
         """
         return len(line) == 79
 
+    def line_generator(self):
+        """
+            The generator used to get the lines of the log file. It concatenates
+            lines that are cut at 79 characters and skips empty lines.
+        """
+        while True:
+            try:
+                line = next(self.line_iter)
+                L = len(line)
+                while L == 79:
+                    txt = next(self.line_iter)
+                    L = len(txt)
+                    line = line + txt
+                if len(line) == 0:
+                    continue
+                else:
+                    yield line
+            except StopIteration:
+                return
+
     def parse (self):
         """
         Parse the log file for relevant information.
         The function returns a generator. Each generated item is a dictionary
         that contains (some of) the following entries:
-        - kind: the kind of information ("error", "box", "ref", "warning")
-        - text: the text of the error or warning
-        - code: the piece of code that caused an error
-        - file, line, last, pkg: as used by Message.format_pos.
+        - type: the type of information ("error", "box", "ref", "warning")
+        - msg:  the error or warning message
+        - context: the piece of code that caused an error or warning
+        - file: the file in which the error occured
+        - line: the line that caused the error or warning
+        - lastline: the last line that caused the error or warning
         """
-        if not self.lines:
-            return
         files = []         # The currently opened files
         parsing = False    # True if we are parsing an error's text
-        skipping = False   # True if we are skipping text until an empty line
         prefix = None  # the prefix for warning messages from packages
-        accu = ""      # accumulated text from the previous line
         macro = None   # the macro in which the error occurs
         cseqs = {}     # undefined control sequences so far
-        for line in self.lines:
 
-            # TeX breaks messages at 79 characters, just to make parsing
-            # trickier...
+        lines = self.line_generator()
 
-            if not parsing and self.continued(line):
-                accu += line
+        for line in lines:
+
+            d = dict()
+            if len(files)>0:
+                d["file"] = files[-1]
+
+            # Errors
+            d["type"] = "error"
+
+            if line[0] == "!":
+                error = line[2:]
+                parsing = True
                 continue
-            line = accu + line
-            accu = ""
 
-            # Text that should be skipped (from bad box messages)
-
-            if prefix is None and line == "":
-                skipping = False
+            if line == "Runaway argument?":
+                error = line
+                parsing = True
                 continue
 
-            if skipping:
-                continue
 
             # Errors (including aborted compilation)
-
-            if parsing:
+            while parsing:
                 if error == "Undefined control sequence.":
                     # This is a special case in order to report which control
                     # sequence is undefined.
                     m = re_cseq.match(line)
                     if m:
                         seq = m.group("seq")
-                        if cseqs.has_key(seq):
+                        if seq in cseqs:
                             error = None
                         else:
                             cseqs[seq] = None
@@ -126,31 +142,18 @@ class LogProcessor(GObject.GObject):
                 m = re_line.match(line)
                 if m:
                     parsing = False
-                    skipping = True
                     pdfTeX = line.find("pdfTeX warning") != -1
                     if error is not None and (pdfTeX or (not pdfTeX)):
                         if pdfTeX:
-                            d = {
-                                "kind": "warning",
-                                "pkg": "pdfTeX",
-                                "text": error[error.find(":")+2:]
-                            }
+                            d["type"] = "warning"
+                            d["msg"] = error[error.find(":")+2:]
                         else:
-                            d =    {
-                                "kind": "error",
-                                "text": error
-                            }
-                        d.update( m.groupdict() )
+                            d["msg"] = error
                         m = re_ignored.search(error)
                         if m:
-                            d["file"] = files[-1]
-                            if d.has_key("code"):
+                            if "code" in d:
                                 del d["code"]
                             d.update( m.groupdict() )
-                        elif files[-1] is None:
-                            d["file"] = files[-1]
-                        else:
-                            d["file"] = files[-1]
                         if macro is not None:
                             d["macro"] = macro
                             macro = None
@@ -159,27 +162,18 @@ class LogProcessor(GObject.GObject):
                     error = line[2:]
                 elif line[0:3] == "***":
                     parsing = False
-                    skipping = True
-                    yield    {
-                        "kind": "abort",
-                        "text": error,
-                        "why" : line[4:],
-                        "file": files[-1]
-                        }
-                continue
+                    d["msg"] = error
+                    yield d
+                line = next(lines)
 
-            if len(line) > 0 and line[0] == "!":
-                error = line[2:]
-                parsing = True
-                continue
+            # End of parsing
 
-            if line == "Runaway argument?":
-                error = line
-                parsing = True
-                continue
+
+
+            # Warnings
+            d["type"] = "warning"
 
             # Long warnings
-
             if prefix is not None:
                 if line[:len(prefix)] == prefix:
                     text.append(line[len(prefix):].strip())
@@ -187,92 +181,63 @@ class LogProcessor(GObject.GObject):
                     text = " ".join(text)
                     m = re_online.search(text)
                     if m:
-                        info["line"] = m.group("line")
+                        d["line"] = m.group("line")
                         text = text[:m.start()] + text[m.end():]
-                    info["text"] = text
-                    d = { "kind": "warning" }
-                    d.update( info )
+                    d["msg"] = text
                     yield d
                     prefix = None
                 continue
 
             # Undefined references
+            d["type"] = "reference"
 
             m = re_reference.match(line)
             if m:
-                d =    {
-                    "kind": "warning",
-                    "text": f"Reference `{m.group('ref')}' undefined.",
-                    "file": files[-1]
-                    }
-                d.update( m.groupdict() )
+                d["msg"] = f"Reference `{m.group('ref')}' undefined."
                 yield d
                 continue
 
             m = re_label.match(line)
             if m:
-                d =    {
-                    "kind": "warning",
-                    "file": files[-1]
-                    }
-                d.update( m.groupdict() )
                 yield d
                 continue
 
-            # Other warnings
-
+            # Other warning
             if line.find("Warning") != -1:
                 m = re_warning.match(line)
                 if m:
                     info = m.groupdict()
-                    info["file"] = files[-1]
                     if info["pkg"] is None:
                         del info["pkg"]
                         prefix = ""
                     else:
                         prefix = ("(%s)" % info["pkg"])
-                    prefix = prefix.ljust(m.start("text"))
-                    text = [info["text"]]
+                    prefix = prefix.ljust(m.start("msg"))
+                    text = [info["msg"]]
                 continue
 
             # Bad box messages
+            d["type"] = "badbox"
 
             m = re_badbox.match(line)
             if m:
-                mpos = { "file": files[-1] }
                 m = re_atline.search(line)
                 if m:
                     md = m.groupdict()
                     for key in "line", "last":
-                        if md[key]: mpos[key] = md[key]
+                        if md[key]: d[key] = md[key]
                     line = line[:m.start()]
-                d =    {
-                    "kind": "warning",
-                    "text": line
-                    }
-                d.update( mpos )
+                d["msg"] = line
                 yield d
-                skipping = True
                 continue
 
             # If there is no message, track source names
-
-            self.update_file(line, files)
-
-    def update_file (self, line, stack):
-        """
-        Parse the given line of log file for file openings and closings and
-        update the list `stack'. Newly opened files are at the end, therefore
-        stack[1] is the main source while stack[-1] is the current one. The
-        first element, stack[0], contains the value None for errors that may
-        happen outside the source.
-        """
-        m = re_file.search(line)
-        while m:
-            if line[m.start()] == '(':
-                stack.append(m.group("file"))
-            else:
-                del stack[-1]
-            line = line[m.end():]
             m = re_file.search(line)
+            while m:
+                if line[m.start()] == '(':
+                    files.append(m.group("file"))
+                else:
+                    del files[-1]
+                line = line[m.end():]
+                m = re_file.search(line)
 
